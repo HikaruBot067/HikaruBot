eb9cf305f7d0dbf973d03a38ea5d54ee1a854436
Hanz Bobot <132050914+OrewaHanz@users.noreply.github.com>
2023-06-10 21:10:41 +0800

    Update info-sewa.js

--- plugins/info-sewa.js
+++ plugins/info-sewa.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
   
-var caption = `Hi, Silahkan pilih opsi di bawah ini.\n*Payment*\n\n*x* Dana: 082221792667\n*x* Pulsa: 082221792667`
+var caption = `Hi, Silahkan pilih opsi di bawah ini.\n*Owner*\n\n*x* First: 601127240988 \n*x* Sec: 601137759884`
 conn.relayMessage(m.chat, 
 { liveLocationMessage: {
   degreesLatitude: 35.685506276233525,
