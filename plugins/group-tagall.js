2b1d050e16389b3e7f7ce3b8ddffea7e7149a55f
Hanz Bobot <132050914+OrewaHanz@users.noreply.github.com>
2023-06-11 17:21:17 +0800

    Update group-tagall.js

--- plugins/group-tagall.js
+++ plugins/group-tagall.js
@@ -1,11 +1,11 @@
 let handler = async(m, { conn, text, participants }) => {
   let teks = `⋙ *TAGALL* ⋘
-\n *${text ? text : 'By 4riessss'}*\n\n`
+\n *${text ? text : 'By MarsBotz'}*\n\n`
 		      	for (let mem of participants) {
 		            teks += `│◦❒ @${mem.id.split('@')[0]}\n`
 				}
                 teks += `
-TagAll Members By wa.me/601127240988`
+TagAll Members By wa.me/60199714550`
                 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 }
 handler.help = ['tagall <pesan>']
